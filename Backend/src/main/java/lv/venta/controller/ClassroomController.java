d77482d7bc61afa03396af0ef8cefbbc77b0a48e
Es <148067931+maris-dukalskis@users.noreply.github.com>
2025-02-06 15:21:38 +0200

    Add: cross origins

--- Backend/src/main/java/lv/venta/controller/ClassroomController.java
+++ Backend/src/main/java/lv/venta/controller/ClassroomController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import lv.venta.model.Classroom;
 import lv.venta.service.IClassroomService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/classroom")
 public class ClassroomController {
 
@@ -51,6 +53,7 @@ public class ClassroomController {
 		try {
 			return new ResponseEntity<Classroom>(classroomService.insertNewClassroom(classroom), HttpStatus.OK);
 		} catch (Exception e) {
+			e.printStackTrace();
 			return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
--- Backend/src/main/java/lv/venta/controller/StudentController.java
+++ Backend/src/main/java/lv/venta/controller/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import lv.venta.model.Student;
 import lv.venta.service.IStudentService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/student")
 public class StudentController {
 
